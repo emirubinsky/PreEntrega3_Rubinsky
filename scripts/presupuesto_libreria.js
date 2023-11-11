5c71e570dcfe59de634ee31bab9be8055242f54c
Emiliano Rubinsky <rubinskyemiliano@gmail.com>
2023-11-11 00:55:30 -0300

    Ultimo cambio dolar api

--- scripts/presupuesto_libreria.js
+++ scripts/presupuesto_libreria.js
@@ -1,118 +1,95 @@
 /*
-    Ejecución que se realiza cada vez que se carga
-    esta librería.
+    Codigo que se ejecutará al inicio.
 */
-
-// Definimos un objeto, y ya de paso le damos una propiedad
-const objetoPresupuestador = {
-    // Defino una variable adentro del objeto
-    CONSTANTE_PRECIO_METRO_EN_DOLARES: 3000
-}
-
-//... y luego agrego (o mejor dicho, ASIGNO) una funcion dentro del objeto
-objetoPresupuestador["ejecutarPresupuestador"] = presupuestacion
-
-
-// Asignamos al boton en el "frontend" el evento "click"
-// ... y que ejecute algunas cosas nuestras
-document.getElementById('btn-simulador').addEventListener('click',
-    function (event) {
-
-        // Con esto evitamos que se haga "submit" del formulario
-        event.preventDefault();
-
-        // Guardamos el titulo del proyecto en el Storage
-        // Store
-        const nombreProyecto = document.getElementById('nombre_proyecto_input').value;
-        localStorage.setItem("nombre_proyecto_storage", nombreProyecto);
-
-        const metrosCuadrados = document.getElementById('metros_cuadrados').value;
-        const cantidadCuotas = document.querySelector('input[name="cantidad_cuotas"]:checked').value;
-        // document.getElementById('cantidad_cuotas').value;
-
-        // Ejecutamos la funcion que esta dentro del objeto-presupuestador
-        objetoPresupuestador.ejecutarPresupuestador(metrosCuadrados, cantidadCuotas)
-    });
+const objetoPresupuestador = {}
+accionesAlCargarLibreria()
 
 /*
  Funciones
 */
 
+async function accionesAlCargarLibreria() {
+    // Defino una variable adentro del objeto
+    // -> que ahora lee datos desde una página que nos brinda un servicio
+    objetoPresupuestador.promesaValorDolarBlue = await getValorDolarBlue()
+    objetoPresupuestador.CONSTANTE_PRECIO_METRO_EN_DOLARES = objetoPresupuestador.promesaValorDolarBlue.compra
 
-function presupuestacion(cantidadMetrosCuadrados, cantidadCuotas) {
-    // Codigo personalizado
-
-    /*alert(`Orgánica.
-    ============================================
-    Simulador Presupuesto de obra por Metro Cuadrado
-    ============================================
-    A continuación ingresará los parámetros de la obra que le gustaría que realicemos en conjunto.
-                          
-    PROMOS 2023
-    - Si tu obra tiene una superficie mayor a 100mt2 entonces tienes 5% descuento
-    - Si tu obra tiene una superficie mayor a 200mt2 entonces tienes 8% descuento
-    ============================================`)
-    */
-    //let seguirTrabajando = true
-
-    // Aquí llamo a la propiedad DENTRO del objeto
-
-    const precioMetroEnDolares = this.CONSTANTE_PRECIO_METRO_EN_DOLARES;
-
-
-    //while (seguirTrabajando) {
-
-    // const cantidadMetrosCuadrados = parseInt(prompt('Ingresar metros cuadrados de la obra a realizar:'))
-
-    // const cantidadCuotas = parseInt(prompt('Ingresar cantidad de cuotas a pagar:\nOpc A: Entre 1 a 3 cuotas: 0% interes.\nOpc B: Entre 4 a 6 cuotas: 15% interes\nOpc C: Entre 7 a 12 cuotas cuotas: 30% interes'))
-
-    // INFO: Codigo reemplazado por una llamada a funcion
-    let descuentoMetrosCuadrados = obtenerDescuentoPorMetros(cantidadMetrosCuadrados)
-
-    // INFO: Codigo reemplazado por una llamada a funcion
-    let interesPorCuotas = obtenerInteresPorCuotas(cantidadCuotas)
-
-    // Calculamos el precio
-    const precioTotalMetraje = precioMetroEnDolares * cantidadMetrosCuadrados
-    const descuentoAlMetraje = descuentoMetrosCuadrados > 0 ?
-        Math.round((descuentoMetrosCuadrados * precioTotalMetraje) / 100) :
-        0
-    const precioTotal = precioTotalMetraje - descuentoAlMetraje // Precio contado
-
-    // Calculamos valor cuotas
-    const precioTotalConInteres = interesPorCuotas > 0 ?
-        precioTotal + Math.round(((interesPorCuotas * precioTotal) / 100)) : // Precio contado
-        precioTotal
-    const valorCadaCuota = Math.round(precioTotalConInteres / cantidadCuotas)
-
-    // Cargando cada cuota dentro de un array.
-    let arrayDeCuotas = []
-    for (let index = 0; index < cantidadCuotas; index++) {
-        arrayDeCuotas.push(valorCadaCuota);
+    objetoPresupuestador["detallesDescuentos"] = {
+        "descuentoPorMas100metros": 5,
+        "descuentoPorMas200metros": 8,
+        "descuentoPorDefecto": 0
     }
 
-    // Mostramos resultados
-    /*
-    alert(`Orgánica.
-============================================
-Simulador Presupuesto - RESULTADOS
-============================================
- 
-TOTAL Mts2         = ${cantidadMetrosCuadrados} Mts2
-PRECIO CONTADO     = ${precioTotal} USD
-VALOR Mts2         = ${precioMetroEnDolares} USD / Mts2
- 
-PRECIO FINANCIADO  = ${precioTotalConInteres} USD
-CANTIDAD CUOTAS    = ${cantidadCuotas} Cuotas
- 
-Continue para ver cada cuota.
- 
-============================================
- 
-Presione ENTER para continuar`)
-*/
 
-    let desgloseCuotas = "";
+    //... y luego agrego (o mejor dicho, ASIGNO) una funcion dentro del objeto
+    objetoPresupuestador["ejecutarPresupuestador"] = presupuestacion
+
+    agregarEventosAlDOM()
+
+}
+
+function agregarEventosAlDOM() {
+    // Asignamos al boton en el "frontend" el evento "click"
+    // ... y que ejecute algunas cosas nuestras
+    document.getElementById('btn-simulador')
+        .addEventListener('click', onClickListenerBotonPresupuesto);
+}
+
+function onClickListenerBotonPresupuesto(event) {
+    // Evitamos que se ejecute el Form-submit por defecto.
+    event.preventDefault();
+
+    // Lectura de datos
+    const nombreProyecto = document.getElementById('nombre_proyecto_input').value;
+    localStorage.setItem("nombre_proyecto_storage", nombreProyecto);
+
+    const metrosCuadrados = parseInt(document.getElementById('metros_cuadrados').value);
+    const cantidadCuotas = parseInt(document.querySelector('input[name="cantidad_cuotas"]:checked').value);
+
+    // Ejecucion de la Promise
+    presupuestacion({ metrosCuadrados, cantidadCuotas })
+        .then((presupuestoResultante) => {
+            // Aquí puedes trabajar con los resultados
+            console.log(presupuestoResultante);
+
+            // Integración con el Toastify
+            mostrarNotificacion("Presupuesto Realizado");
+
+            // Integración con la página
+            mostrarElPresupuestoEnHTML(presupuestoResultante)
+        })
+        .catch((error) => {
+            console.error(error);
+
+            // Usamos destructuring del error
+            const { message } = error
+
+            // Integración con el Toastify
+            // + Template literal
+            mostrarNotificacion(`Un Error ha ocurrido. Detalles: ${message}`);
+        });
+}
+
+/**
+ * Ejemplo de UN presupuestoResultante...
+ * {
+    "cantidadMetrosCuadrados": "111",
+    "precioTotal": 98068,
+    "precioMetroEnDolares": 930,
+    "precioTotalConInteres": 98068,
+    "cantidadCuotas": "1",
+    "arrayDeCuotas": [
+        98068
+    ],
+    "nombreProyecto": "test"
+}
+De aqui solo necesitamos ARRAY DE CUOTAS, 
+entonces aca aplicamos destructuring.
+ */
+// Definimos una funcion que va a aplicar el DESTRUCTURING en el procesamiento de parametros
+function mostrarElPresupuestoEnHTML({
+    arrayDeCuotas,
+}){
     let numeroCuota = 1;
 
     const tableBody = document.getElementById('tableBody');
@@ -129,39 +106,77 @@ Presione ENTER para continuar`)
     const nombreProyectoDesdeElStorage = localStorage.getItem("nombre_proyecto_storage");
     document.getElementById('nombre_proyecto_visual').innerHTML = nombreProyectoDesdeElStorage;
 
-    /*
-        alert(`Orgánica.
-    ============================================
-    Simulador Presupuesto - Desglosa cuotas
-    ============================================
-    
-    ${desgloseCuotas}
-    
-    ============================================
-    
-    Presione ENTER para continuar`)
-*/
-    // const teclaApretada = prompt('Simulacion finalizada.\nPresione X para salir.')
-
-    // seguirTrabajando = teclaApretada !== 'X'
-
-    // }
-
-
 }
+
+// Función para mostrar notificaciones con Toastify
+function mostrarNotificacion(mensaje) {
+    Toastify({
+        text: mensaje,
+        duration: 3000, // Duración en milisegundos
+        gravity: "center", // Posición de la notificación (top, bottom, center)
+        position: "center", // Alineación horizontal (left, center, right)
+        backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)", // Color de fondo
+        stopOnFocus: true, // La notificación se cierra cuando se enfoca en ella
+    }).showToast();
+}
+
+
+// Info: probamos el renombre metrosCuadrados >> a cantidadMetrosCuadrados
+function presupuestacion({ metrosCuadrados: cantidadMetrosCuadrados, cantidadCuotas }) {
+    return new Promise((resolve, reject) => {
+        const { CONSTANTE_PRECIO_METRO_EN_DOLARES } = objetoPresupuestador;
+
+        let descuentoMetrosCuadrados = obtenerDescuentoPorMetros(cantidadMetrosCuadrados);
+        let interesPorCuotas = obtenerInteresPorCuotas(cantidadCuotas);
+
+        const precioTotalMetraje = CONSTANTE_PRECIO_METRO_EN_DOLARES * cantidadMetrosCuadrados;
+        const descuentoAlMetraje = descuentoMetrosCuadrados > 0 ?
+            Math.round((descuentoMetrosCuadrados * precioTotalMetraje) / 100) : //<- TODO: potencial arrow function de dos parametros
+            0;
+        const precioTotal = precioTotalMetraje - descuentoAlMetraje;
+
+        const precioTotalConInteres = interesPorCuotas > 0 ?
+            precioTotal + obtenerPrecioTotal(interesPorCuotas, precioTotal) : //<- uso de arrow function
+            precioTotal;
+        const valorCadaCuota = obtenerValorCadaCuota(precioTotalConInteres, cantidadCuotas); //<- uso de arrow function
+
+        let arrayDeCuotas = [];
+        for (let index = 0; index < cantidadCuotas; index++) {
+            arrayDeCuotas.push(valorCadaCuota);
+        }
+
+        const nombreProyectoDesdeElStorage = localStorage.getItem("nombre_proyecto_storage");
+
+        const resultados = {
+            cantidadMetrosCuadrados,
+            precioTotal,
+            precioMetroEnDolares: CONSTANTE_PRECIO_METRO_EN_DOLARES,
+            precioTotalConInteres,
+            cantidadCuotas,
+            arrayDeCuotas,
+            nombreProyecto: nombreProyectoDesdeElStorage
+        };
+
+        resolve(resultados);
+    });
+}
+
+// Uso de arrow functions "nombradas" (o named arrow functions)
+obtenerPrecioTotal = (interesPorCuotas, precioTotal) => Math.round(((interesPorCuotas * precioTotal) / 100));
+obtenerValorCadaCuota = (precioTotalConInteres, cantidadCuotas) => (Math.round(precioTotalConInteres / cantidadCuotas));
 
 function obtenerDescuentoPorMetros(cantidadMetrosCuadrados) {
 
     if (cantidadMetrosCuadrados >= 200) {
-        return 8
+        return objetoPresupuestador["detallesDescuentos"].descuentoPorMas200metros
     }
 
     if (cantidadMetrosCuadrados >= 100) {
-        return 5
+        return objetoPresupuestador["detallesDescuentos"].descuentoPorMas100metros
     }
 
     // retorno por defecto
-    return 0
+    return objetoPresupuestador["detallesDescuentos"].descuentoPorDefecto
 
 }
 
@@ -171,7 +186,7 @@ function obtenerInteresPorCuotas(cantidadCuotas) {
 
     // Hasta 3 cuotas, nada.
     if (cantidadCuotas <= 3) {
-        interesPorCuotas = 0
+        interesPorCuotas = 0  //<-- aca se podría hacer lo mismo, y leer de json.
     } else {
         if (cantidadCuotas >= 4 && cantidadCuotas <= 6) {
             interesPorCuotas = 15
@@ -184,4 +199,36 @@ function obtenerInteresPorCuotas(cantidadCuotas) {
 
     return Math.round(interesPorCuotas)
 }
+
+async function getValorDolarBlue() {
+    // URL de la API que deseas consultar
+    const apiUrl = 'https://dolarapi.com/v1/dolares/blue';
+
+    try {
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} - ${response.statusText}`);
+        }
+
+        const data = await response.json();
+
+        // Mostrar el valor del dólar en algún elemento HTML
+
+        const valorDolarElement = document.getElementById('valorDolar');
+        valorDolarElement.innerHTML = `<strong>Valor del Dólar Blue:</strong> <strong>$${data.compra}</strong> (Compra), <strong>$${data.venta}</strong> (Venta)`;
+
+
+
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+}
+
+// Llamamos a la función y mostramos el valor en los elementos HTML
+getValorDolarBlue().catch(error => {
+    console.error('Error:', error.message);
+});
 
